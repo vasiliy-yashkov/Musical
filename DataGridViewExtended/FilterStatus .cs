f24f85a9404b4e6ef232f7a463ac888bf40995a6
Vasiliy Yashkov <vasiliy.yashkov@red-soft.biz>
2018-05-22 12:41:06 +0300

    Add extended data grid with filter header

---
+++ DataGridViewExtended/FilterStatus .cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataGridViewExtended
+{
+    class FilterStatus
+    {
+        public string columnName { get; set; }
+        public string valueString { get; set; }
+        public string formatedValue { get; set; }
+        public bool check { get; set; }
+    }
+}
